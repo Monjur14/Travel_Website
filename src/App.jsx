6bcbed005314e33cf87868c5e56c7e2163d5fa79
Monjur Hossen <14monjurhossen@gmail.com>
2023-11-26 12:46:00 +0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Navbar from './Components/Navbar'
+import AnimatedText from './Sections/AnimatedText'
 import Hero from './Sections/Hero'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar/>
       <Hero/>
+      {/* <AnimatedText/> */}
     </>
   )
 }
--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
     <div className="contain section_padding w-full h-screen flex flex-col lg:flex-row gap-10 lg:gap-0">
       <div className="space-y-4 xsm:space-y-6 xxl:space-y-10 basis-1/2">
         <p className="text-orange font-bold text-lg xsm:text-xl md:text-2xl xxl:text-[2.1rem]">Best Destinations around the world</p>
-        <h1 className="font-volkhov text-[2.9rem] xsm:text-[3.3rem] md:text-[5.3rem] lg:text-[5rem] leading-tight font-extrabold xxl:text-[6.5rem]">Travel, <span className="relative z-30">enjoy <img src="Decore1.png" alt="" className="w-96 h-2 absolute bottom-2 left-0 -z-30"/></span> and live a new and full life</h1>
+        <h1 className="font-volkhov text-[2.9rem] xsm:text-[3.8rem] md:text-[5.3rem] lg:text-[5rem] leading-tight font-extrabold xxl:text-[6.5rem]">Travel, <span className="relative z-30">enjoy <img src="Decore1.png" alt="" className="w-96 h-2 absolute bottom-2 left-0 -z-30"/></span> and live a new and full life</h1>
         <p className="text-base text-gray font-poppins font-medium xsm:text-lg md:text-xl xxl:text-2xl">Built Wicket longer admire do barton vanity itself do in it. Preferred to sportsmen it engrossed listening. Park gate sell they west hard for the.</p>
         <div className="flex gap-5 xxl:gap-8">
             <div><button className="px-4 py-3 xsm:px-6 xsm:py-4 xxl:px-8 xxl:py-5 bg-yellow rounded-lg text-white box_shadow font-poppins text-lg xxl:text-xl">Find out more</button></div>
@@ -14,8 +14,8 @@ const Hero = () => {
             </div>
         </div>
       </div>
-      <div className="basis-1/2">
-        <img src="Decore.png" alt="" className="absolute lg:top-0 lg:right-0 -z-40 xxl:w-[55rem]"/>
+      <div className="basis-1/2 mx-auto">
+        <img src="Decore.png" alt="" className="absolute lg:top-0 lg:right-0 -z-40 w-full xxl:w-[55rem]"/>
         <img src="Traveller 1.png" alt="" className="mt-8 md:mt-12 lg:mt-0" />
       </div>
     </div>
