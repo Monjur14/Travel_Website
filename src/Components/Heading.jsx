d021f2370669c4ae67babc2ee0b33ea3f72b289c
Monjur Hossen <14monjurhossen@gmail.com>
2023-11-30 05:18:02 +0600

    commit

--- src/Components/Heading.jsx
+++ src/Components/Heading.jsx
@@ -1,17 +1,21 @@
 import { motion} from "framer-motion"
+import { useMediaQuery } from 'react-responsive'
 
 const Heading = ({tag, title}) => {
+  const isMoblie = useMediaQuery({
+    query: '(min-width: 550px)'
+  })
   return (
-    <motion.div  className="text-center overflow-hidden  py-2 space-y-1 xsm:space-y-2 lg:space-y-4 xxl:space-y-6">
+    <motion.div  className="text-center overflow-hidden py-2 space-y-1 xsm:space-y-2 lg:space-y-4 xxl:space-y-6">
         <motion.h3
         initial={{opacity: 0, scale: 1.3}} whileInView={{opacity: 1, scale: 1, transition: {duration: 1.5}}}  viewport={{once: true, margin: "-20%"}}
         className="font-poppins text-gray text-lg xsm:text-[1.3rem] md:text-2xl xxl:text-4xl"
         >{tag}</motion.h3>
         <motion.h1
-        initial={{y: 280, rotateZ: 55, opacity: 0}}
+        initial={{ y: isMoblie ? 280 : 170, rotateZ: 55, opacity: 0}}
         whileInView={{y: 0, rotateZ: 0, opacity: 1}}
-        transition={{duration: 1.5}} viewport={{once: true, margin: "-20%" }}
-        className="font-volkhov text-black font-bold text-2xl xsm:text-[1.8rem] md:text-4xl lg:text-[2.7rem] xxl:text-[3.2rem]"
+        transition={{duration: 1.5}} viewport={{once: true, margin: "-20%"}}
+        className="font-volkhov text-black font-bold text-2xl xsm:text-[1.8rem] md:text-4xl lg:text-[2.7rem] xxl:text-[3.2rem] z-50"
         >{title}</motion.h1>
     </motion.div>
   )
--- src/Components/TravelComp.jsx
+++ src/Components/TravelComp.jsx
@@ -1,7 +1,11 @@
 import { IoPaperPlaneSharp } from "react-icons/io5";
+import {motion} from "framer-motion"
+
 const TravelComp = ({bg, des, price, day}) => {
   return (
-    <div className="flex z-50 flex-col h-[30rem] md:h-[20rem] lg:w-full lg:h-[30rem] md:flex-row lg:flex-col rounded-3xl box_shadow cursor-pointer hover:scale-105 transition-all duration-300">
+    <motion.div
+    initial={{scale: 0.8, y: 60, opacity: 0}} whileInView={{scale: 1, y: 0, opacity: 1, transition: {duration: 1.5, type: "tween", ease: "easeInOut"}}} viewport={{once: true, margin: "-15%"}}
+    className="flex z-50 flex-col h-[30rem] md:h-[20rem] lg:w-full lg:h-[30rem] md:flex-row lg:flex-col rounded-3xl box_shadow cursor-pointer hover:scale-105 transition-all duration-300">
       <div className={`w-full overflow-hidden basis-3/4 md:basis-1/2 lg:basis-3/4 rounded-tl-3xl rounded-tr-3xl ${bg} md:rounded-bl-3xl lg:rounded-bl-none md:rounded-tr-none lg:rounded-tl-3xl lg:rounded-tr-3xl`}></div>
       <div className="font-poppins z-50 p-4 md:basis-1/2 lg:basis-1/4 flex flex-col justify-between">
         <div className="flex justify-between text-lg font-semibold md:text-xl w-full">
@@ -15,7 +19,7 @@ const TravelComp = ({bg, des, price, day}) => {
           <p className="text-gray md:text-lg">{day}</p>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
